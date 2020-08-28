475e30b76d5753152e355b3501337740c2e2f08d
taiwocoker <taiwofcoker@gmail.com>
2020-08-28 01:00:52 +0100

    Add home page dom actions

--- src/home_dom_actions.js
+++ src/home_dom_actions.js
@@ -1,3 +1,19 @@
-class HomeDomActions  {
-
-}
+const mainPage = () => {
+    const main = document.createElement('div');
+    main.classList.add('bg-image');
+  
+    const headerTitle = document.createElement('h1');
+    headerTitle.innerText = 'We provide excellent customer service and quality meals';
+    const button = document.createElement('button');
+    const buttonText = document.createElement('a');
+    buttonText.classList.add('btn-menu');
+    buttonText.innerText = 'VIEW DELICACIES';
+    button.appendChild(buttonText);
+  
+    main.appendChild(headerTitle);
+    main.appendChild(button);
+  
+    return main;
+  };
+  
+  export default mainPage;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-alert('Hello there')
+import header from "./navbar_dom_actions";
+import mainPage from "./home_dom_actions";
+
+const navbar = header();
+
+const homePage = mainPage();
+
+const content = document.querySelector("#content");
+
+const show = (section) => {
+  content.innerHTML = "";
+  content.appendChild(navbar);
+  content.appendChild(section);
+};
+
+show(homePage);
+
