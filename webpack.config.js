4ea5d0c94f8f2e6edf475b1470b5a219c4b5dec7
taiwocoker <taiwofcoker@gmail.com>
2020-08-30 10:00:50 +0100

    Fix bug on images

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,17 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+
 };
